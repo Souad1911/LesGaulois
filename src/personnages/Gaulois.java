e6beb68cf59082772628aa7633b5ddc2b8d8e5e4
KRS3737A <KRS3737A@u3-216-05.ens.fsi>
2022-09-30 09:03:26 +0200

    premier Commit

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Gaulois {
+     private String nom;
+     private int force;
+     private int effetPotion=1;
+     
+	public Gaulois(String nom, int force, int effetPotion) {
+		super();
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+}
